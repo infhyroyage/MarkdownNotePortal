9771e650269a76d6a317587df9ed06d0236918e3
Takeru KIDO <keidy2463@gmail.com>
2025-09-15 15:20:55 +0900

    Add SPA

---
+++ spa/src/App.tsx
@@ -0,0 +1,12 @@
+/**
+ * アプリケーションのエントリーポイント
+ * @returns アプリケーションのエントリーポイント
+ */
+export default function App() {
+  return (
+    <>
+      <h1>Markdown Note Portal</h1>
+      <div>HelloWorld</div>
+    </>
+  );
+}
