f2f27c1da71fe351f230fc4c162a7ad3c7322bcd
Takeru KIDO <keidy2463@gmail.com>
2025-10-22 01:13:25 +0900

    Add dynamodb local

--- lambdas/layer/python/utils.py
+++ lambdas/layer/python/utils.py
@@ -1,5 +1,7 @@
 """ユーティリティ関数"""
 
+import os
+
 import boto3
 
 
@@ -9,4 +11,16 @@ def get_dynamodb_client():
     Returns:
         boto3.client: DynamoDBクライアント
     """
+    endpoint_url = os.environ.get("DYNAMODB_ENDPOINT")
+    if endpoint_url:
+        # ローカル環境のDynamoDB(DynamoDB Local)
+        return boto3.client(
+            "dynamodb",
+            endpoint_url=endpoint_url,
+            region_name="fakeRegion",
+            aws_access_key_id="fakeMyKeyId",
+            aws_secret_access_key="fakeSecretAccessKey",
+        )
+
+    # AWS環境のDynamoDB
     return boto3.client("dynamodb")
--- lambdas/tests/layer/test_utils.py
+++ lambdas/tests/layer/test_utils.py
@@ -6,12 +6,36 @@ from lambdas.layer.python.utils import get_dynamodb_client
 
 
 @patch("lambdas.layer.python.utils.boto3.client")
-def test_get_dynamodb_client(mock_boto3_client):
-    """get_dynamodb_client関数のテスト"""
+@patch("lambdas.layer.python.utils.os.environ.get")
+def test_get_dynamodb_client_aws_environment(mock_env_get, mock_boto3_client):
+    """get_dynamodb_client関数のテスト(AWS環境)"""
+    mock_env_get.return_value = None
     sentinel = object()
     mock_boto3_client.return_value = sentinel
 
     client = get_dynamodb_client()
 
+    mock_env_get.assert_called_once_with("DYNAMODB_ENDPOINT")
     mock_boto3_client.assert_called_once_with("dynamodb")
+    assert client is sentinel
+
+
+@patch("lambdas.layer.python.utils.boto3.client")
+@patch("lambdas.layer.python.utils.os.environ.get")
+def test_get_dynamodb_client_local_environment(mock_env_get, mock_boto3_client):
+    """get_dynamodb_client関数のテスト(ローカル環境)"""
+    mock_env_get.return_value = "http://dynamodb-local:8000"
+    sentinel = object()
+    mock_boto3_client.return_value = sentinel
+
+    client = get_dynamodb_client()
+
+    mock_env_get.assert_called_once_with("DYNAMODB_ENDPOINT")
+    mock_boto3_client.assert_called_once_with(
+        "dynamodb",
+        endpoint_url="http://dynamodb-local:8000",
+        region_name="fakeRegion",
+        aws_access_key_id="fakeMyKeyId",
+        aws_secret_access_key="fakeSecretAccessKey",
+    )
     assert client is sentinel
