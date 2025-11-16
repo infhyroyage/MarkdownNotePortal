8a0b2b804717fa15eb09fd8794915ffa6944efd1
Takeru KIDO <keidy2463@gmail.com>
2025-11-16 18:13:08 +0900

    Update lint setting in lambdas

--- lambdas/eslint.config.js
+++ lambdas/eslint.config.js
@@ -1,20 +1,17 @@
 import js from "@eslint/js";
-import tsParser from "@typescript-eslint/parser";
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ["**/*.ts"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.node,
-      parser: tsParser,
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
     },
   },
 ]);
