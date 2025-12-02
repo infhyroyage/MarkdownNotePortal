e0d071c8828ff864454abc3d62dbe0ddc8694a65
Takeru KIDO <keidy2463@gmail.com>
2025-12-03 02:01:33 +0900

    Fix vite build config in lambdas

--- lambdas/vite.build.config.ts
+++ lambdas/vite.build.config.ts
@@ -50,11 +50,9 @@ export default defineConfig({
         banner: `
 import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const require = createRequire(import.meta.url);
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
         `.trim(),
       },
       external: ["crypto"],
